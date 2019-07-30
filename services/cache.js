dd6d754af6ce3073dd92ff357b00f7c4204eac3e
Sahil Wasan <sahilwasan000@gmail.com>
2019-07-30 13:31:02 +0530

    Added Redis structure

--- services/cache.js
+++ services/cache.js
@@ -1,11 +1,16 @@
 const mongoose = require('mongoose');
+const redis = require('redis');
+const util = require('util');
 
+const redisUrl = 'redis://127.0.0.1:6379';
+const client = redis.createClient(redisUrl);
+client.get = util.promisify(client.get);
 const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function() {
 
   console.log('just run a query');
-  
+
   const key = Object.assign({}, this.getQuery(), {
     collection: this.mongooseCollection.name
   });
