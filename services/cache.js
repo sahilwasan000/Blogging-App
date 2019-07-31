be1f2a4828fd5004d2a357bc4c7af90dc95f4b67
Sahil Wasan <sahilwasan000@gmail.com>
2019-07-31 11:59:40 +0530

    Initialized Redis

--- services/cache.js
+++ services/cache.js
@@ -5,6 +5,7 @@ const util = require('util');
 const redisUrl = 'redis://127.0.0.1:6379';
 const client = redis.createClient(redisUrl);
 client.get = util.promisify(client.get);
+
 const exec = mongoose.Query.prototype.exec;
 
 mongoose.Query.prototype.exec = function() {
