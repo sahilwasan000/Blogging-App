bc4b264b5e7586cd65dfee64e92dd4a97900f2dd
Sahil Wasan <sahilwasan000@gmail.com>
2019-08-15 13:50:29 +0530

    Update passport.js

--- services/passport.js
+++ services/passport.js
@@ -23,6 +23,7 @@ passport.use(
       clientSecret: keys.googleClientSecret,
       proxy: true
     },
+
     async (accessToken, refreshToken, profile, done) => {
       try {
         const existingUser = await User.findOne({ googleId: profile.id });
